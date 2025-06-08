79dc106d4e3e4932729b60a754c80fc46b04bafc
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2025-06-08 12:49:52 -0400

    Added a simple Kusto proxy

---
+++ azure-data-explorer-datasource-proxy/Program.cs
@@ -0,0 +1,74 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+var logger = app.Logger;
+
+var endpointToQuery = Environment.GetEnvironmentVariable("KUSTO_ENDPOINT");
+if (string.IsNullOrWhiteSpace(endpointToQuery))
+{
+    logger.LogError("KUSTO_ENDPOINT environment variable is not set.");
+    return;
+}
+
+app.MapPost(
+    "/v1/rest/query",
+    async context =>
+    {
+        using var httpClient = new HttpClient();
+
+        var restrictedHeaders = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "Content-Type",
+            "Content-Length",
+            "Content-Encoding",
+            "Transfer-Encoding",
+            "Host",
+        };
+
+        foreach (var header in context.Request.Headers)
+        {
+            if (restrictedHeaders.Contains(header.Key))
+                continue;
+
+            try
+            {
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation(
+                    header.Key,
+                    header.Value.ToArray()
+                );
+            }
+            catch
+            {
+                logger.LogError(
+                    "Failed to add header {HeaderKey} with value {HeaderValue} to HttpClient.",
+                    header.Key,
+                    string.Join(", ", header.Value)
+                );
+            }
+        }
+
+        using var reader = new StreamReader(context.Request.Body);
+        var requestBody = await reader.ReadToEndAsync();
+        var content = new StringContent(requestBody, System.Text.Encoding.UTF8, "application/json");
+
+        var response = await httpClient.PostAsync($"{endpointToQuery}/v1/rest/query", content);
+
+        context.Response.StatusCode = (int)response.StatusCode;
+
+        foreach (var header in response.Headers)
+        {
+            context.Response.Headers[header.Key] = header.Value.ToArray();
+        }
+
+        foreach (var header in response.Content.Headers)
+        {
+            context.Response.Headers[header.Key] = header.Value.ToArray();
+        }
+
+        context.Response.Headers.Remove("transfer-encoding");
+
+        var responseStream = await response.Content.ReadAsStreamAsync();
+        await responseStream.CopyToAsync(context.Response.Body);
+    }
+);
+
+app.Run();
---
+++ azure-data-explorer-datasource-proxy/README.md
@@ -0,0 +1,14 @@
+# ADX Proxy
+
+> TODO
+
+Run from PowerShell:
+
+```powershell
+$GIT_ROOT = git rev-parse --show-toplevel
+cd "$GIT_ROOT\azure-data-explorer-datasource-proxy"
+
+$env:KUSTO_ENDPOINT = "https://rakirahman.westus2.kusto.windows.net"
+$env:ASPNETCORE_URLS = "http://localhost:5005"
+dotnet run
+```
